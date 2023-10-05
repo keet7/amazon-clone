a532b5b4bf426cc6286c7f05282de31e0b368f41
keet7 <keerthana.pepeti@gamil.com>
2023-10-05 16:25:02 +0530

    add the starting files

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAVQVojnuMF2xd-360dPQVVeKgUhgsQhd4",
+  authDomain: "challenge-962a1.firebaseapp.com",
+  projectId: "challenge-962a1",
+  storageBucket: "challenge-962a1.appspot.com",
+  messagingSenderId: "285137272423",
+  appId: "1:285137272423:web:7266395a916bd1d244dec2",
+  measurementId: "G-ZYYGK2X08K",
+};
